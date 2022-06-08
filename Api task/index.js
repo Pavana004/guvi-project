552976f8455a76f411cb66fa0f9d9814719cb04c
Pavana004 <101943293+Pavana004@users.noreply.github.com>
2022-06-08 22:29:18 +0530

    Add files via upload

--- Api task/index.js
+++ Api task/index.js
@@ -1,26 +1,27 @@
+
 const result = document.querySelector(".card");
-const Api = "https://api.artic.edu/api/v1/artworks";
+const Api_2="https://emojihub.herokuapp.com/api/all";
 
 
-fetch(Api)
-    .then((res) => res.json())
-    .then((data) => showData(data.data))
-    .catch((error)=>console.log(error));
+fetch(Api_2)
+.then((res)=> res.json())
+.then((data)=>showEmoji(data))
+.catch((error)=>console.log(error));
 
-const showData = (data)=>{
+const showEmoji = (data)=>{
     
-    data.map(i=>{
+    data.map(item=>{
+        
         const div = document.createElement("div");
         div.className = "card text-center ";
-        div.innerHTML = `  <h5 class="card-title">Id:${i.id}</h5>
+        div.innerHTML = `  <h5 class="card-title">Name : ${item.name}</h5>
          <div class="card-body">
-           <h5 class="card-title1">Api_Model:${i.api_model}</h5>
-           <h5 class="card-title1">Api_Link:${i.api_link}</h5>
+          <p>Category : ${item.category}</p>
+          <p>Group : ${item.group}</p>
          </div>`;
   
-        result.appendChild(div)
+         result.appendChild(div)
     });
     
 }
-
 
