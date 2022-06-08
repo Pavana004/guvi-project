3379cbdc96f31eef8bddf58075b937f3fca06d12
Pavana004 <101943293+Pavana004@users.noreply.github.com>
2022-06-08 22:11:20 +0530

    Update index.js

--- Api task/index.js
+++ Api task/index.js
@@ -24,26 +24,3 @@ const showData = (data)=>{
 }
 
 
-// const Api_2="https://emojihub.herokuapp.com/api/all";
-// const emoji = document.querySelector(".emoji");
-
-// fetch(Api_2)
-// .then((res)=> res.json())
-// .then((data)=>showEmoji(data))
-// // .catch((error)=>console.log(error));
-
-// const showEmoji = (data)=>{
-    
-//     data.map(item=>{
-//         const div = document.createElement("div");
-//         div.className = "card text-center ";
-//         div.innerHTML = `  <h5 class="card-title">Name:${item.name}</h5>
-//          <div class="card-body">
-          
-//          </div>`;
-  
-//          result.appendChild(div)
-//     });
-    
-// }
-
